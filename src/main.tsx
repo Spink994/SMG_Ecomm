45f83367b65eaf68fa85fe9dc834aec999e3eb64
spink994 <ayodeji.bakare994@gmail.com>
2023-11-01 08:16:57 +0100

    service worker

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,31 @@ import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<PersistGate
-				loading={null}
-				persistor={persistor}
-			>
-				<Suspense fallback={null}>
-					<RouterProvider router={router} />
-				</Suspense>
-			</PersistGate>
-		</Provider>
-	</React.StrictMode>
-);
+import { Workbox } from 'workbox-window';
+
+function RootApp() {
+	// change this when testing in dev
+	const isProduction = process.env.NODE_ENV === 'production';
+
+	// in the App component:
+	React.useEffect(() => {
+		if (isProduction && 'serviceWorker' in navigator) {
+			const wb = new Workbox('./service-worker.js');
+			wb.register();
+		}
+	}, []);
+
+	return (
+		<React.StrictMode>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<Suspense fallback={null}>
+						<RouterProvider router={router} />
+					</Suspense>
+				</PersistGate>
+			</Provider>
+		</React.StrictMode>
+	);
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<RootApp />);
